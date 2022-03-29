3c28f6ab6e1698327fedc9ca2815c03584a9ea39
spfkeh <83802641+spfkeh@users.noreply.github.com>
2022-03-29 18:52:06 +0900

    a

--- Assets/02.Script/Monster/BossPatten/Meteo.cs
+++ Assets/02.Script/Monster/BossPatten/Meteo.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class Meteo : MonoBehaviour
 {
     [SerializeField] private float speed;
+    private void Start()
+    {
+        Invoke("DestroyObject", 3f);
+    }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector2.left * speed);
+    }
+    void DestroyObject()
+    {
+        Destroy(gameObject);
     }
 }
