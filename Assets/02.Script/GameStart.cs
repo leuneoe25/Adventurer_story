6f9235aaaee0910599b0472d62eacc7d62b8325d
spfkeh <83802641+spfkeh@users.noreply.github.com>
2022-04-02 19:26:29 +0900

    ㅊ

--- Assets/02.Script/GameStart.cs
+++ Assets/02.Script/GameStart.cs
@@ -8,6 +8,14 @@ public class GameStart : MonoBehaviour
     public void gameStart()
     {
         SceneManager.LoadScene("InGame");
+        PlayerPrefs.SetInt("ESkill", 0);
+        PlayerPrefs.SetInt("QSkill", 0);
+        PlayerPrefs.SetInt("Coin", 0);
+        PlayerPrefs.SetInt("Level", 1);
+        if(GameSystem.instans()!=null)
+        {
+            GameSystem.instans().SetisFirst(true);
+        }
     }
     public void GameQuit()
     {
